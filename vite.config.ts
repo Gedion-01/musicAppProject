6329acca8103db506996fd1fe8dffa54b494d931
gedion <gedion@domain.com>
2024-02-20 15:58:41 +0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import cssOnly from 'rollup-plugin-css-only';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    {
-      ...cssOnly({ output: 'style.css' }), // specify the output CSS file
-      apply: 'build' // Apply only in the build phase
-    }],
+  plugins: [react()],
 })
