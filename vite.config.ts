1ee5433b273f3f87eecf05ad6dd8d665ffcfaead
gedion <gedion@domain.com>
2024-02-20 15:37:12 +0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import cssOnly from 'rollup-plugin-css-only';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    {
+      ...cssOnly({ output: 'style.css' }), // specify the output CSS file
+      apply: 'build' // Apply only in the build phase
+    }],
 })
